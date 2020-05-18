e88462548b6b17361fc1d36a689af41a96192c66
Miruna Rucareanu <miruna.rucareanu@endava.com>
2020-05-18 12:20:02 +0300

    Initial commit

---
+++ GitIntegration/FirstClassInAWhile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitIntegration
+{
+    public class FirstClassInAWhile
+    {
+        public int Nr { get; set; }
+
+        public void WhatCanIDoForYou()
+        {
+
+        }
+    }
+}
